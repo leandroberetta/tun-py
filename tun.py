298cbe22f62a66cf10033beb9d09b4cbfa8f7992
Leandro Beretta <lea.beretta@gmail.com>
2017-02-05 11:35:14 -0300

    Several improvements. It has to be tested yet.

--- tun.py
+++ tun.py
@@ -1,22 +1,67 @@
 import subprocess
 import time
+import re
+
+
+class Tunnel:
+
+    def __init__(self, dst_port, src_host, src_port, from_host):
+        self.ssh_config = ['ssh', '-o', 'ConnectTimeout=10', '-N', '-L']
+
+        self.dst_port = dst_port
+        self.src_host = src_host
+        self.src_port = src_port
+        self.from_host = from_host[:-1] # Removes an ending \n
+        self.process = None
+
+        self.ssh_config.append('{0}:{1}:{2}'.format(self.dst_port, self.src_host, self.src_port))
+        self.ssh_config.append(self.from_host)
+
+    def establish(self):
+        if self.process and self.process.poll():
+            self.process.terminate()
+
+        self.process = subprocess.Popen(self.ssh_config)
+
+    def terminate(self):
+        self.process.terminate()
+
+    def is_alive(self):
+        return self.process.poll() is None
+
+    def __str__(self):
+        return 'tunnel: from: {0:16} : {1:6} -> to: localhost : {2:6} '.format(self.src_host, self.src_port, self.dst_port)
+
 
 if __name__ == '__main__':
-    ssh_config = ['ssh', '-N', '-L']
-    processes = []
+    tunnels = []
 
-    with open('tun.cfg') as tunnels:
-        for tunnel in tunnels.readlines():
-            tun_config = tunnel.split()
+    def valid_tunnel(tunnel_line):
+        return not tunnel_line.startswith('#') and not tunnel_line.startswith('\n')
 
-            if not tunnel.startswith('#') and not tunnel.startswith('\n'):
-                processes.append(subprocess.Popen(ssh_config + tun_config))
+    def tunnel_config_parser(tunnel_line):
+        return re.split('[: ]', tunnel_line)
+
+    with open('tun.cfg') as tunnels_config_file:
+        tunnel_lines = filter(lambda tunnel_line: valid_tunnel(tunnel_line), tunnels_config_file.readlines())
+
+        for tunnel_line in tunnel_lines:
+            tunnel = Tunnel(*tunnel_config_parser(tunnel_line))
+            tunnel.establish()
+            tunnels.append(tunnel)
+
+            print(str(tunnel))
 
     while True:
         try:
+            for tunnel in tunnels:
+                if not tunnel.is_alive():
+                    print('restarting ' + str(tunnel))
+                    tunnel.establish()
+
             time.sleep(1)
         except KeyboardInterrupt:
-            for process in processes:
-                process.terminate()
+            for tunnel in tunnels:
+                tunnel.terminate()
 
             exit()
